a97e067d5ba4288723b0330c3322b7567ae7158a
Priti-Tayde <prititayde39@gmail.com>
2026-05-21 14:38:56 +0530

    Initial commit

---
+++ database/db_manager.py
@@ -0,0 +1,10 @@
+def create_tables():
+    print("Tables created")
+
+def register_user(username, password):
+    # insert user into database
+    pass
+
+def login_user(username, password):
+    # validate user
+    pass
